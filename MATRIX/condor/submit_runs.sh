b50f7d27f395414125482cfdbeebded410cca73a
jacoterh <54140851+jacoterh@users.noreply.github.com>
2024-11-12 17:00:25 +0000

    swapping inner and outer loop in submission script

--- MATRIX/condor/submit_runs.sh
+++ MATRIX/condor/submit_runs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
-INPUT_DIR=/data/theorie/jthoeve/physics_projects/matrix-alle-hawaii2/MATRIX/run/ppttx20_MATRIX/input/
+INPUT_DIR=/data/theorie/jthoeve/physics_projects/matrix-alle-hawaii2/MATRIX/run/ppttx20_MATRIX/input
 HTCONDOR_THRESHOLD=100
 MASSES_MT=("172.5" "170.0" "175.0")
+PROCESSES=("ATLAS_TTBAR_13TEV_LJ_DIF" "ATLAS_TTBAR_13TEV_HADR_DIF_MTTBAR-YTTBAR" "ATLAS_TTBAR_8TEV_DIF" "CMS_TTBAR_13TEV_2L_DIF" "CMS_TTBAR_5TEV_TOT_X-SEC" "CMS_TTBAR_7TEV_TOT_X-SEC" "CMS_TTBAR_8TEV_2L_DIF")
 
 check_htcondor_jobs() {
     # Get the number of jobs in the HTCondor queue
@@ -11,53 +12,68 @@ check_htcondor_jobs() {
     echo $current_jobs
 }
 
-# loop over datasets
-for PROCESS_DIR in $INPUT_DIR/*; do
+delay_until_space() {
+  # checks whether the number of jobs in the queue is below the threshold before moving to the next run
+  while true; do
+     num_current_jobs=$(check_htcondor_jobs)
+     # if the number of jobs is below the threshold, break the loop
+     if [[ $num_current_jobs -lt $HTCONDOR_THRESHOLD ]]; then
+        break  # break out of the while loop
+     fi
+     echo $num_current_jobs " jobs in the queue, waiting till this is below " $HTCONDOR_THRESHOLD
+     sleep 900  # Check every 15 min
+  done
 
-    PROCESS=$(basename "$PROCESS_DIR")
+  return 0
+}
 
-    # skip non-ttbar processes
-    [[ "$PROCESS" != *"TTBAR"* || "$PROCESS" == *"run"* ]] && continue
 
-    # loop over masses
-    index=0
-    for MT in "${MASSES_MT[@]}"; do
+# loop over mass
+for MT in "${MASSES_MT[@]}"; do
 
-      MODEL_FILE=$PROCESS_DIR/"model.dat"
+    pids=()  # Array to store PIDs of the background processes
+    echo "Starting mass run for mt = $MT"
+
+    for PROCESS in "${PROCESSES[@]}"; do
+      echo "Starting process $PROCESS"
+
+      PROCESS_DIR=$INPUT_DIR/$PROCESS
+      MODEL_FILE=$INPUT_DIR/"run_"$PROCESS/"model.dat"
 
       # the first mass run includes the full run
-      if [[ $index -eq 0 ]]; then
-        ./bin/run_process "run_"$PROCESS --run_mode run --input_dir $PROCESS
-
-        # uncomment option after first mass run to avoid including the pre-in results in the subsequent mass runs
-        sed -i '/^#include_pre_in_results = 0/s/^#//' $INPUT_DIR/"run_"$PROCESS/parameter.dat
+      if [[ $MT == "172.5" && "$PROCESS" == "ATLAS_TTBAR_13TEV_LJ_DIF" ]]; then
+        ./bin/run_process "run_"$PROCESS --run_mode run_pre_and_main --input_dir $PROCESS &
+        sleep 600  # wait for 10 min before starting the next process
+        pids+=($!)
+        delay_until_space
+      elif [[ $MT == "172.5" ]]; then
+        ./bin/run_process "run_"$PROCESS --run_mode run --input_dir $PROCESS &
+        pids+=($!)
+        delay_until_space
       else
         # update the top mass
         sed -i "s/^\(\s*6\s*\)[^ ]*/\1$MT/" "$MODEL_FILE"
 
-        # run the main stage only. We run in the background and continue to the next run if the number of jobs is
-        # below the threshold
+        # run the main stage only (we already have a successful run).
         ./bin/run_process "run_"$PROCESS --run_mode run_main &
-
-        # wait until the number of jobs is below the threshold
-        while true; do
-           num_current_jobs=$(check_htcondor_jobs)
-           # if the number of jobs is below the threshold, break the loop
-           if [[ $num_current_jobs -lt $HTCONDOR_THRESHOLD ]]; then
-              break
-           fi
-          sleep 900  # Check every 15 min
-        done
+        pids+=($!)  # store the process id
+        delay_until_space
       fi
-
-      if [[ $? -ne 0 ]]; then
-          echo "Command failed on $PROCESS and $MT Exiting loop."
-          break  # Exit the loop if the command failed
-      fi
-      ((index++))
-
     done
-#  break
+
+    # Wait for all background jobs to finish before moving to the next mass point
+    for pid in "${pids[@]}"; do
+        wait $pid  # Wait for each process in the array
+        echo "Command with PID $pid has finished"
+    done
+    echo "All processes for mass $MT have finished"
+    echo "Uncommenting the pre-in results for the next mass run"
+
+    # uncomment the pre-in results for the next mass run
+    for PROCESS in "${PROCESSES[@]}"; do
+      sed -i '/^#include_pre_in_results = 0/s/^#//' $INPUT_DIR/"run_"$PROCESS/parameter.dat
+    done
+
 done
 
 
