e7dac90f93c5047b9fbc0e1b9061d17850243e64
jacoterh <54140851+jacoterh@users.noreply.github.com>
2025-08-13 15:14:25 +0200

    adding yttbar case

--- scripts/mc_unc_to_commondata.py
+++ scripts/mc_unc_to_commondata.py
@@ -57,7 +57,12 @@ dataset_inputs = {
 "CMS_TTBAR_13TEV_TOT_X-SEC"
 }
 
+# the following datasets were computed only for yttbar > 0, but the exp data also contains yttbar <0
+# so treat these as a special case
+yttbar_reflected = ["CMS_TTBAR_8TEV_LJ_DIF_YTTBAR-NORM", "CMS_TTBAR_13TEV_2L_DIF_YTTBAR-NORM"]
+
 matrix_suffix = "__NNLO_QCD"
+matrix_prefix_2D = "2D_"
 fb_to_pb = 1e-3
 matrix_run_dir = pathlib.Path("../MATRIX/final_results")
 commondata_dir = pathlib.Path("/Users/jaco/Documents/physics_projects/nnpdf/nnpdf_data/nnpdf_data/commondata")
@@ -91,13 +96,10 @@ def write_commondata_unc(mc_uncs, commondata_path, observable_name):
     """
     Writes the Monte Carlo uncertainties to uncertainties.yaml in commondata format
     """
-    mc_dict = {"definitions": {"stat":
+    mc_dict = {"definitions": {"mc_stat":
                                    {"description": "Monte Carlo uncertainty on the mtop variations",
                                     "treatment": "ADD",
-                                    "type": "UNCORR"}}, "bins": [{"stat": mc_unc} for mc_unc in mc_uncs]}
-
-    with open(commondata_path /  "mc_top_uncertainties.yaml", 'w') as file:
-        yaml.dump(mc_dict, file)
+                                    "type": "UNCORR"}}, "bins": [{"mc_stat": mc_unc} for mc_unc in mc_uncs]}
 
     # link to metadata
     with open(commondata_path / "metadata.yaml") as file:
@@ -106,7 +108,14 @@ def write_commondata_unc(mc_uncs, commondata_path, observable_name):
     for obs in metadata["implemented_observables"]:
         if obs["observable_name"] == observable_name:
             data_uncertainties = obs["data_uncertainties"]
-            data_uncertainties_variant = data_uncertainties + ["mc_top_uncertainties.yaml"]
+
+            # associate the observable name to the mc uncertainty file
+            unc_suffix = data_uncertainties[0].split('uncertainties_')[1]
+            mc_unc_filename = f"mc_top_uncertainties_{unc_suffix}"
+            with open(commondata_path / mc_unc_filename, 'w') as file:
+                yaml.dump(mc_dict, file)
+
+            data_uncertainties_variant = data_uncertainties + [mc_unc_filename]
             if "variants" in obs:
                 obs["variants"]["mc_uncertainties"] = {"data_uncertainties": data_uncertainties_variant}
             else:
@@ -145,14 +154,6 @@ for dataset in dataset_inputs:
     if "TOT_X-SEC" in dataset:
         continue
 
-    # TODO: catr dist
-    if "YTTBAR" in dataset:
-        continue
-
-    # TODO: 2D dist
-    if observable_name.count("-") > 1:
-        continue
-
     sqrts = int(dataset.split("_")[2].replace("TEV", ""))
     mt_vals = [170, 175]
 
@@ -164,8 +165,12 @@ for dataset in dataset_inputs:
 
             # find the uncertainty in each bin and the sum over bins
             dataset_abs = dataset.split("-NORM")[0]
-
-            matrix_filename = dataset_abs + matrix_suffix
+            if dataset_abs.count("-") > 0:  # 2D dist
+                matrix_filename = matrix_prefix_2D + dataset_abs + matrix_suffix
+            elif "YTTBAR" in dataset:  # dist in yttbar
+                matrix_filename = dataset_abs + "_catr" + matrix_suffix
+            else:
+                matrix_filename = dataset_abs + matrix_suffix
 
             if matrix_filename in dfs_all[f'run_ttb_{sqrts}tev_mt_{mt_val}'].keys():
                 df = dfs_all[f'run_ttb_{sqrts}tev_mt_{mt_val}'][matrix_filename]
@@ -176,7 +181,12 @@ for dataset in dataset_inputs:
     try:
         delta_y_i_avg = np.sqrt(0.5 * (delta_y_i_mt[170] ** 2 + delta_y_i_mt[175] ** 2))
     except KeyError:
-        import pdb; pdb.set_trace()
+        # TODO: 2D grids with bm and gm as suffix are not recognized
+        continue
+
+    # 1D dist in yttbar
+    if dataset in yttbar_reflected:
+        delta_y_i_avg = pd.concat([delta_y_i_avg.iloc[::-1], delta_y_i_avg], ignore_index=True)
 
     # write to commondata folder
 
