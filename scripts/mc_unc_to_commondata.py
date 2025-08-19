91812080ab90867322ecb232441c816c60fedfa1
jacoterh <54140851+jacoterh@users.noreply.github.com>
2025-08-19 15:50:40 +0100

    binwidth fix in error propagation

--- scripts/mc_unc_to_commondata.py
+++ scripts/mc_unc_to_commondata.py
@@ -57,6 +57,38 @@ dataset_inputs = {
 "CMS_TTBAR_13TEV_TOT_X-SEC"
 }
 
+pt_test = np.array([0.002639,
+0.005891,
+0.005954,
+0.004422,
+0.002644,
+0.001431,
+6.64500000e-04,
+3.16700000e-04,
+1.66100000e-04,
+7.39400000e-05,
+4.75e-05,
+1.85e-05,
+6.65100000e-06,
+2.15900000e-06,
+6.50600000e-07,
+7.87200000e-08,
+])
+
+bin_width = np.array([40, 40, 40, 40, 40, 50, 50, 50, 50, 50, 50, 100, 100, 150, 150, 500])
+
+mtt_test_exp = np.array([1.98085000e-03, 3.99172000e-03, 2.00949000e-03, 6.36295000e-04, 1.43808000e-04, 2.71952000e-05])
+mtt_test_theory = np.array([1.2951925e0, 3.3421510e0, 1.7010997e0,5.4686545e-1,1.3677893e-1,2.6385245e-2,2.4443162e-3])
+mtt_integrated = 8.2024355e2
+mtt_test_theory_norm = mtt_test_theory / mtt_integrated
+ratio = mtt_test_exp / mtt_test_theory_norm[:-1]
+# I need to do mtt_test_theory/m
+
+bin_width_mtt = np.array([80, 90, 150, 200, 280, 400])
+
+# finding: np.sum(pt_test * bin_width) = 1
+
+
 # the following datasets were computed only for yttbar > 0, but the exp data also contains yttbar <0
 # so treat these as a special case
 yttbar_reflected = ["CMS_TTBAR_8TEV_LJ_DIF_YTTBAR-NORM", "CMS_TTBAR_13TEV_2L_DIF_YTTBAR-NORM"]
@@ -79,18 +111,31 @@ def read_table(table):
 
 def convert_unc_abs_to_norm(df):
     """
-    propagates the error on x_i to y_i = x_i / S with S = sum(x_i)
+    propagates the error
     """
-    x_i = df["scale-central"]
-    delta_x_i = df["central-error"]
-    S = x_i.sum(axis=0)
-    sum_delta_xi_sq = (delta_x_i ** 2).sum(axis=0)
+    x_i = df["scale-central"] # \Delta sigma_i / bin_width
+    err_x_i = df["central-error"] # err (\Delta sigma_i / bin_width)
+    if "left-edge" in df.columns and "right-edge" in df.columns:
+        bin_width = df["right-edge"] - df["left-edge"]
+    else:
+        try:
+            bin_width_1 = df["right-edge1"] - df["left-edge1"]
+            bin_width_2 = df["right-edge2"] - df["left-edge2"]
+            bin_width = bin_width_1 * bin_width_2
+        except KeyError:
+            import pdb; pdb.set_trace()
+
+    delta_sigma_i = x_i * bin_width
+    err_delta_sigma_i = err_x_i * bin_width
+
+    S = delta_sigma_i.sum(axis=0)
+    sum_err_delta_sigma_i_sq = (err_delta_sigma_i ** 2).sum(axis=0)
 
     # propagate error
-    delta_y_i_sq = (delta_x_i ** 2 - 2 * x_i * delta_x_i ** 2 / S + x_i ** 2 / S ** 2 * sum_delta_xi_sq) / S ** 2
-    delta_y_i = np.sqrt(delta_y_i_sq)
-    y_i = x_i / S
-    return y_i, delta_y_i
+    err_sq_delta_sigma_i_norm = (err_delta_sigma_i ** 2 - 2 * delta_sigma_i * err_delta_sigma_i ** 2 / S + delta_sigma_i ** 2 / S ** 2 * sum_err_delta_sigma_i_sq) / S ** 2
+    err_delta_sigma_i_norm = np.sqrt(err_sq_delta_sigma_i_norm)
+
+    return delta_sigma_i / S / bin_width, err_delta_sigma_i_norm / bin_width
 
 def write_commondata_unc(mc_uncs, commondata_path, observable_name):
     """
@@ -168,6 +213,7 @@ for dataset in dataset_inputs:
 
     sqrts = int(dataset.split("_")[2].replace("TEV", ""))
     mt_vals = [170, 175]
+    #mt_vals = ["172P5"]
 
     # collect MC for each mt variation
     delta_y_i_mt = {}
@@ -179,7 +225,7 @@ for dataset in dataset_inputs:
             dataset_abs = dataset.split("-NORM")[0]
             if dataset_abs.count("-") > 0:  # 2D dist
                 matrix_filename = matrix_prefix_2D + dataset_abs + matrix_suffix
-            elif "YTTBAR" in dataset:  # dist in yttbar
+            elif dataset in yttbar_reflected:  # dist in yttbar
                 matrix_filename = dataset_abs + "_catr" + matrix_suffix
             else:
                 matrix_filename = dataset_abs + matrix_suffix
@@ -194,30 +240,50 @@ for dataset in dataset_inputs:
                 df = pd.concat(dfs, ignore_index=True)
             # the bins in ATLAS_TTBAR_13TEV_LJ_DIF_MTTBAR-PTT-NORM need to be merged first
             elif dataset == "ATLAS_TTBAR_13TEV_LJ_DIF_MTTBAR-PTT-NORM":
+
                 matrix_filename_bm = matrix_filename.replace("__NNLO_QCD", f"_bm__NNLO_QCD")
                 df_bm = dfs_all[f'run_ttb_{sqrts}tev_mt_{mt_val}'][matrix_filename_bm]
-                df_bm = df_bm[["scale-central", "central-error"]]
+                df_bm = df_bm[["scale-central", "central-error", "left-edge1", "right-edge1",
+                               "left-edge2", "right-edge2"]]
                 merged_bins = [(0, 1), (2, 3), (4, 7), (8, 8), (9, 10), (11, 13), (14, 15), (16, 16), (17, 18),
                                (19, 20), (21, 22), (23, 23), (24, 27), (28, 29), (30, 31)]
+
                 scale_central_merged = [df_bm.loc[start:end, "scale-central"].sum() for start, end in merged_bins]
                 central_error_merged = [np.sqrt((df_bm.loc[start:end, "central-error"] ** 2).sum()) for start, end in merged_bins]
-                df = pd.DataFrame({"scale-central": scale_central_merged, "central-error": central_error_merged})
+                bin_merged_1 = [(df_bm.loc[start, "left-edge1"], df_bm.loc[end, "right-edge1"]) for start, end in merged_bins]
+                bin_merged_2 = [(df_bm.loc[start, "left-edge2"], df_bm.loc[end, "right-edge2"]) for start, end in merged_bins]
+                left_edge1_merged = [bin_merged_1[i][0] for i in range(len(bin_merged_1))]
+                right_edge1_merged = [bin_merged_1[i][1] for i in range(len(bin_merged_1))]
+                left_edge2_merged = [bin_merged_2[i][0] for i in range(len(bin_merged_2))]
+                right_edge2_merged = [bin_merged_2[i][1] for i in range(len(bin_merged_2))]
+
+
+                df = pd.DataFrame({"scale-central": scale_central_merged, "central-error": central_error_merged,
+                                   "left-edge1": left_edge1_merged, "right-edge1": right_edge1_merged,
+                                   "left-edge2": left_edge2_merged, "right-edge2": right_edge2_merged})
             elif matrix_filename in dfs_all[f'run_ttb_{sqrts}tev_mt_{mt_val}'].keys():
                 df = dfs_all[f'run_ttb_{sqrts}tev_mt_{mt_val}'][matrix_filename]
 
-            _, delta_y_i = convert_unc_abs_to_norm(df)
+
+            y_i, delta_y_i = convert_unc_abs_to_norm(df)
+
             delta_y_i_mt[mt_val] = delta_y_i
 
     # average mc unc in top variations
     try:
         delta_y_i_avg = np.sqrt(0.5 * (delta_y_i_mt[170] ** 2 + delta_y_i_mt[175] ** 2))
     except KeyError:
-        # TODO: 2D grids with bm and gm as suffix are not recognized
+        print(f"Skipping dataset {dataset}")
         continue
+    #delta_y_i_avg = delta_y_i_mt["172P5"]
+
+
 
     # 1D dist in yttbar
     if dataset in yttbar_reflected:
         delta_y_i_avg = pd.concat([delta_y_i_avg.iloc[::-1], delta_y_i_avg], ignore_index=True)
+
+
 
     # write to commondata folder
 
